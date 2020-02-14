c01b4a99f977ae0344a515a1ec9073a38ab004a9
yangxiushan <501942821@qq.com>
2020-02-14 14:50:46 +0800

    auto sign test_2

--- sign/sign.php
+++ sign/sign.php
@@ -1,26 +1,45 @@
 <?php
-$signRoot="/var/www/test/";//linux
-$signPath=$signRoot."tool/ausign";//linux
-$certPath=$signRoot."dev.p12";
-$mobileProvisionPath=$signRoot."dev.mobileprovision";
-$ipaPath=$signRoot."ipa/game.ipa";
-$saveIpaPath=$signRoot."resigned/game.ipa";
-$certPassword="123456";
-$loginCmd=$signPath.' --email 2462611616@qq.com -p 123456';
-$signCmd=$signPath.' --sign '.$ipaPath." -c ".$certPath." -m ".$mobileProvisionPath." -p ".$certPassword." -o ".$saveIpaPath;
-$addTimeSignCmd=$signCmd." -at 20190901";//签名并插入锁的命令
+//$signRoot="/var/www/test/";//linux
+//$signPath=$signRoot."tool/ausign";//linux
+//$certPath=$signRoot."dev.p12";
+//$mobileProvisionPath=$signRoot."dev.mobileprovision";
+//$ipaPath=$signRoot."ipa/game.ipa";
+//$saveIpaPath=$signRoot."resigned/game.ipa";
+//$certPassword="123456";
+//$loginCmd=$signPath.' --email 2462611616@qq.com -p 123456';
+//$signCmd=$signPath.' --sign '.$ipaPath." -c ".$certPath." -m ".$mobileProvisionPath." -p ".$certPassword." -o ".$saveIpaPath;
+//$addTimeSignCmd=$signCmd." -at 20190901";//签名并插入锁的命令
+//
+//exec($loginCmd,$outputString,$loginStatus);
+//if($loginStatus!=0){
+//    echo "login fail <br/>";
+//    echo var_dump($outputString);
+//}else{
+//    echo "login success <br/>";
+//    exec($signCmd,$outputString,$signStatus);
+//    if($signStatus!=0){
+//        echo "sign fail <br/>";
+//        echo var_dump($outputString);
+//    }else{
+//        echo "sign success,ipa saved to ".$saveIpaPath;
+//    }
+//}
 
-exec($loginCmd,$outputString,$loginStatus);
-if($loginStatus!=0){
-    echo "login fail <br/>";
-    echo var_dump($outputString);
-}else{
-    echo "login success <br/>";
-    exec($signCmd,$outputString,$signStatus);
-    if($signStatus!=0){
-        echo "sign fail <br/>";
-        echo var_dump($outputString);
-    }else{
-        echo "sign success,ipa saved to ".$saveIpaPath;
-    }
-}
+$isignPath = "/www/isign/isign";
+$iosUdidPath = "/www/ios_udid/sign/";
+$pemPath = "/root/.isign/";
+
+$c = $pemPath . "certificate.pem";
+$k = $pemPath . "key.pem";
+$p = $iosUdidPath . "sigmDemo_adhoc.mobileprovision";
+$out = $iosUdidPath . "resigned3.ipa";
+$in = $iosUdidPath . "SignDemo.ipa";
+
+$exec = "$isignPath -c $c -k $k -p $p -o $out $in -v";
+
+
+system($exec, $status);
+
+var_dump(111111111111111);
+var_dump($status);
+var_dump(1111111111111111);
